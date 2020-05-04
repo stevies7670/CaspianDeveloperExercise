d3a6114bdc09f2bda78fe874d78fd7ab08246753
stevenwilson <stevendwilson15@gmail.com>
2020-05-04 19:49:28 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CaspianDeveloperExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
