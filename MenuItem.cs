5f8ac04a16e9c15d4f3d786bd689ea627ee38c23
stevenwilson <stevendwilson15@gmail.com>
2020-05-04 20:37:21 +0100

    Adding simple tests to ensure that the MenuItem object returns all of the necessary values

---
+++ MenuItem.cs
@@ -0,0 +1,17 @@
+﻿namespace CaspianDeveloperExercise
+{
+    public class MenuItem
+    {
+        public string Name { get; set; }
+
+        public ENUM_Temp Temp { get; set; }
+
+        public double Price {get; set;}
+    }
+
+    public enum ENUM_Temp
+    {
+        Hot,
+        Cold
+    }
+}
